06062fc3c9a2ff2224697a5deb9088e44c5570ab
O-keita <o.keita@alustudent.com>
2024-11-14 15:31:54 +0000

    ready for deploy

--- MarAcademy/MarAcademy/wsgi.py
+++ MarAcademy/MarAcademy/wsgi.py
@@ -1,20 +1,13 @@
-"""
-WSGI config for MarAcademy project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 import sys
 
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+# Add the parent directory of the 'MarAcademy' project to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MarAcademy.settings")
 
+# Expose the WSGI application
 application = get_wsgi_application()
