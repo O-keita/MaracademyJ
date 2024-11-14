bebaca6ec63d7dd806dc8646b77b33fd9346acda
O-keita <o.keita@alustudent.com>
2024-11-14 15:25:57 +0000

    ready for deploy

--- MarAcademy/MarAcademy/wsgi.py
+++ MarAcademy/MarAcademy/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MarAcademy.MarAcademy.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MarAcademy.settings")
 
 application = get_wsgi_application()
