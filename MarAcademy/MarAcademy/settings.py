d2c9a61ed45e0b40eacd1091919f03609aa6edc6
O-keita <o.keita@alustudent.com>
2024-11-14 14:47:21 +0000

    ready for deploy

--- MarAcademy/MarAcademy/settings.py
+++ MarAcademy/MarAcademy/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import dj_database_url
+import MarAcademy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,6 +62,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "MarAcademy.wsgi.application"
+
+DJANGO_SETTINGS_MODULE=MarAcademy.settings
+
 
 # Database
 DATABASES = {
