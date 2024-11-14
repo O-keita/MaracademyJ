2e47bcb57ec02634d9c09d33e23c84bcb40371b3
O-keita <o.keita@alustudent.com>
2024-11-14 16:06:26 +0000

    ready for deploy

--- MarAcademy/MarAcademy/urls.py
+++ MarAcademy/MarAcademy/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,4 +28,6 @@ urlpatterns = [
     path("", include("courses.urls")),
     path("", include("assignments.urls")),
     path("", include("progress.urls"))
-]
+
+
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
